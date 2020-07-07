6c424c8fad151a48ed061e7e686afcf584d64b22
Arindam Bose <arindam.1993@gmail.com>
2020-07-07 09:49:33 -0700

    setup gh-pages branch

--- index.js
+++ index.js
@@ -1,221 +1,392 @@
-import {
-  Scene,
-  WebGLRenderer,
-  PerspectiveCamera,
-  Mesh,
-  BufferGeometry,
-  BufferAttribute,
-  ShaderMaterial,
-  TextureLoader,
-  FrontSide,
-  AmbientLight,
-  DirectionalLight,
-  ShaderLib,
-  UniformsUtils,
-  UniformsLib,
-  NearestFilter,
-  MeshBasicMaterial
-} from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { getPixels, decodeDEM, maxHeight } from "./utils";
-import Martini from "@mapbox/martini";
-import pomVert from "./shaders/pom.vertex.glsl";
-import pomFrag from "./shaders/pom.fragment.glsl";
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
 
-const scene = new Scene();
-const camera = new PerspectiveCamera(
-  90,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-const renderer = new WebGLRenderer();
-const canvas = renderer.domElement;
-const controls = new OrbitControls(camera, canvas);
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(canvas);
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
 
-const metersPerPixel = 124.73948277849482;
-const terrainScale = 0.01;
-const terrainExaggeration = 3;
-camera.position.set(
-  metersPerPixel * terrainScale,
-  metersPerPixel * terrainScale,
-  400
-);
-camera.lookAt(0, 0, 0);
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
 
-const demPromise = new Promise((resolve, reject) => {
-  new TextureLoader().load(
-    "https://api.mapbox.com/v4/mapbox.terrain-rgb/12/687/1583@2x.png?access_token=pk.eyJ1IjoiYXJpbmRhbTE5OTMiLCJhIjoiY2p0bnU2dmtoMHp4ZTN5cGxmZXJpa3BpdiJ9._9GLi1K1ERIMzwpzWSL-PA",
-    texture => {
-      resolve(texture);
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
     }
-  );
-});
 
-const rasterPromise = new Promise((resolve, reject) => {
-  new TextureLoader().load(
-    "https://api.mapbox.com/v4/mapbox.satellite/12/687/1583@2x.png?access_token=pk.eyJ1IjoiYXJpbmRhbTE5OTMiLCJhIjoiY2p0bnU2dmtoMHp4ZTN5cGxmZXJpa3BpdiJ9._9GLi1K1ERIMzwpzWSL-PA",
-    texture => {
-      resolve(texture);
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
     }
-  );
-});
 
-Promise.all([demPromise, rasterPromise]).then(([texture, rasterTexture]) => {
-  const pixels = getPixels(texture);
-  const decodedDem = decodeDEM(pixels);
-  let max = -1;
-  for (let i = 0; i < decodedDem.length; i++) {
-    if (decodedDem[i] > max) {
-      max = decodedDem[i];
+    function resolve(x){
+      return modules[name][1][x] || x;
     }
   }
 
-  const martini = new Martini(513);
-  // generate RTIN hierarchy from terrain data (an array of size^2 length)
-  const tile = martini.createTile(decodedDem);
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
 
-  // get a mesh (vertices and triangles indices) for a 50m error
-  const martiniMesh = tile.getMesh(200);
-  // const numVertices = martiniMesh.vertices.length / 2;
-  const numTriangles = martiniMesh.triangles.length / 3;
-  // build up vertex and uv buffer
-  // for (let i = 0; i < numVertices; i++) {
-  //   const x = martiniMesh.vertices[2 * i];
-  //   const y = martiniMesh.vertices[2 * i + 1];
-  //   const z = decodedDem[y * 513 + x];
-
-  //   vertices[3 * i] = x * metersPerPixel * terrainScale;
-  //   vertices[3 * i + 1] = y * metersPerPixel * terrainScale;
-  //   vertices[3 * i + 2] = z * terrainScale * terrainExaggeration + 10;
-
-  //   uv[2 * i] = x / 513;
-  //   uv[2 * i + 1] = 1 - y / 513;
-  // }
-
-  //duplicate vertices so that we can have flat triangles
-  const vertices = new Float32Array(numTriangles * 3 * 3);
-  const planeVertices = new Float32Array(numTriangles * 3 * 3);
-  const indices = new Uint16Array(numTriangles * 3);
-
-  let vertCtr = 0;
-  const pushTriangle = i => {
-    // debugger;
-    const v1 = martiniMesh.triangles[3 * i];
-    const v2 = martiniMesh.triangles[3 * i + 1];
-    const v3 = martiniMesh.triangles[3 * i + 2];
-
-    const v1x = martiniMesh.vertices[2 * v1];
-    const v1y = martiniMesh.vertices[2 * v1 + 1];
-    const v1z = decodedDem[v1y * 513 + v1x];
-
-    const v2x = martiniMesh.vertices[2 * v2];
-    const v2y = martiniMesh.vertices[2 * v2 + 1];
-    const v2z = decodedDem[v2y * 513 + v2x];
-
-    const v3x = martiniMesh.vertices[2 * v3];
-    const v3y = martiniMesh.vertices[2 * v3 + 1];
-    const v3z = decodedDem[v3y * 513 + v3x];
-
-    const v1xf = v1x * metersPerPixel * terrainScale;
-    const v1yf = v1y * metersPerPixel * terrainScale;
-    const v1zf = v1z * terrainScale * terrainExaggeration + 10;
-
-    const v2xf = v2x * metersPerPixel * terrainScale;
-    const v2yf = v2y * metersPerPixel * terrainScale;
-    const v2zf = v2z * terrainScale * terrainExaggeration + 10;
-
-    const v3xf = v3x * metersPerPixel * terrainScale;
-    const v3yf = v3y * metersPerPixel * terrainScale;
-    const v3zf = v3z * terrainScale * terrainExaggeration + 10;
-
-    const cxf = (v1xf + v2xf + v3xf) / 3;
-    const cyf = (v1yf + v2yf + v3yf) / 3;
-    const czf = (v1zf + v2zf + v3zf) / 3;
-
-    // Add each vertex
-    vertices[3 * vertCtr] = v1xf;
-    vertices[3 * vertCtr + 1] = v1yf;
-    vertices[3 * vertCtr + 2] = v1zf;
-    planeVertices[3 * vertCtr] = cxf;
-    planeVertices[3 * vertCtr + 1] = cyf;
-    planeVertices[3 * vertCtr + 2] = czf;
-    indices[3 * i] = vertCtr;
-    vertCtr++;
-
-    vertices[3 * vertCtr] = v3xf;
-    vertices[3 * vertCtr + 1] = v3yf;
-    vertices[3 * vertCtr + 2] = v3zf;
-    planeVertices[3 * vertCtr] = cxf;
-    planeVertices[3 * vertCtr + 1] = cyf;
-    planeVertices[3 * vertCtr + 2] = czf;
-    indices[3 * i + 1] = vertCtr;
-    vertCtr++;
-
-    vertices[3 * vertCtr] = v2xf;
-    vertices[3 * vertCtr + 1] = v2yf;
-    vertices[3 * vertCtr + 2] = v2zf;
-    planeVertices[3 * vertCtr] = cxf;
-    planeVertices[3 * vertCtr + 1] = cyf;
-    planeVertices[3 * vertCtr + 2] = czf;
-    indices[3 * i + 2] = vertCtr;
-    vertCtr++;
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
   };
 
-  for (let i = 0; i < numTriangles; i++) {
-    pushTriangle(i);
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
   }
 
-  const geometry = new BufferGeometry();
-  geometry.setAttribute("position", new BufferAttribute(vertices, 3));
-  geometry.setAttribute("planePosition", new BufferAttribute(planeVertices, 3));
-  geometry.setIndex(new BufferAttribute(indices, 1));
-  geometry.computeBoundingBox();
-  geometry.computeVertexNormals();
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
 
-  let uniforms = UniformsUtils.clone(ShaderLib.standard.uniforms);
-  uniforms["heightmap"] = { value: texture };
-  uniforms["maxHeight"] = { value: max * terrainScale * terrainExaggeration };
-  uniforms["map"] = { value: rasterTexture };
-  uniforms["terrainScale"] = { value: terrainScale };
-  uniforms["exaggeration"] = { value: terrainExaggeration };
-  uniforms["cPos"] = { value: camera.getWorldPosition() };
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
 
-  const material = new ShaderMaterial({
-    uniforms,
-    lights: true,
-    vertexShader: pomVert,
-    fragmentShader: pomFrag
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"C:/Users/Arindam-PC/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+var bundleURL = null;
+
+function getBundleURLCached() {
+  if (!bundleURL) {
+    bundleURL = getBundleURL();
+  }
+
+  return bundleURL;
+}
+
+function getBundleURL() {
+  // Attempt to find the URL of the current script and use that as the base URL
+  try {
+    throw new Error();
+  } catch (err) {
+    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+
+    if (matches) {
+      return getBaseURL(matches[0]);
+    }
+  }
+
+  return '/';
+}
+
+function getBaseURL(url) {
+  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)\/[^/]+$/, '$1') + '/';
+}
+
+exports.getBundleURL = getBundleURLCached;
+exports.getBaseURL = getBaseURL;
+},{}],"C:/Users/Arindam-PC/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
+var bundle = require('./bundle-url');
+
+function updateLink(link) {
+  var newLink = link.cloneNode();
+
+  newLink.onload = function () {
+    link.remove();
+  };
+
+  newLink.href = link.href.split('?')[0] + '?' + Date.now();
+  link.parentNode.insertBefore(newLink, link.nextSibling);
+}
+
+var cssTimeout = null;
+
+function reloadCSS() {
+  if (cssTimeout) {
+    return;
+  }
+
+  cssTimeout = setTimeout(function () {
+    var links = document.querySelectorAll('link[rel="stylesheet"]');
+
+    for (var i = 0; i < links.length; i++) {
+      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
+        updateLink(links[i]);
+      }
+    }
+
+    cssTimeout = null;
+  }, 50);
+}
+
+module.exports = reloadCSS;
+},{"./bundle-url":"C:/Users/Arindam-PC/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"C:/Users/Arindam-PC/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51461" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
   });
-  const mesh = new Mesh(geometry, material);
-  const wireframe = new Mesh(
-    geometry,
-    new MeshBasicMaterial({ wireframe: true })
-  );
-  scene.add(mesh);
-  scene.add(wireframe);
+}
 
-  // Add lights
-  const ambient = new AmbientLight(0x404040); // soft white
-  scene.add(ambient);
-  const directional = new DirectionalLight(0x404040, 3);
-  directional.position.set(1, 1, 1);
-  scene.add(directional);
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
 
-  window.onresize = () => {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  };
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
 
-  const onRenderFrame = () => {
-    controls.update();
-    camera.updateMatrixWorld();
-    uniforms["cPos"] = { value: camera.getWorldPosition() };
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
 
-    renderer.render(scene, camera);
-    window.requestAnimationFrame(onRenderFrame);
-  };
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
 
-  window.requestAnimationFrame(onRenderFrame);
-});
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["C:/Users/Arindam-PC/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
+//# sourceMappingURL=/index.js.map
